4e3847492b0ad23595db36ae1d9379784cb0dd2c
AlejandroSanchez54 <115221762+AlejandroSanchez54@users.noreply.github.com>
2022-12-09 19:59:28 +0100

    Added last three options of the administrator
    Added the method rename_media, upload_file (some issues) and remove_file

--- iceflix/cliente.py
+++ iceflix/cliente.py
@@ -16,6 +16,7 @@ class Client(Ice.Application):
     def __init__(self):
         self.token = ""
         self.media = []
+        self.mainService= None
 
     def run(self,argv):
 
@@ -49,7 +50,7 @@ class Client(Ice.Application):
         else:
             print("Connected to the mainService.")
 
-    def autenthicate_user(self, mainService):
+    def autenthicate_user(self):
         if self.token!="":
             print("Error. You are loggin in. \n")
         else:
@@ -61,7 +62,7 @@ class Client(Ice.Application):
                 print("Unexpected error has ocurred. You should introduce a username and a password")
             else:
                 try:
-                    authenticator = mainService.getAuthenticator()
+                    authenticator = self.mainService.getAuthenticator()
                     self.token = authenticator.refreshAuthorization(username,hash_password)
                     return True
                 except IceFlix.Unauthorized:
@@ -69,12 +70,12 @@ class Client(Ice.Application):
                 except:
                     print("Unexpected error has ocurred.")
 
-    def search_name(self, mainService, exact):
+    def search_name(self,exact):
         print("Enter name to search: \n" )
         name = input()
 
         try:
-            catalog = mainService.getCatalog()
+            catalog = self.mainService.getCatalog()
             media = catalog.getTilesByName(name,exact)
             if media != []:
                 for i in len(media):
@@ -94,11 +95,11 @@ class Client(Ice.Application):
         except IceFlix.WrongMediaId:
             print("Wrong media Id.\n")
 
-    def search_tag(self, mainService, includeAllTags):
+    def search_tag(self,includeAllTags):
         print("Enter the name of the tags: \n")
         tags = input().split()
         try:
-            catalog = mainService.getCatalog()
+            catalog = self.mainService.getCatalog()
             media = catalog.getTilesByTags(tags,includeAllTags, self.token)
             if media != []:
                 for i in len(media):
@@ -118,13 +119,13 @@ class Client(Ice.Application):
         except IceFlix.WrongMediaId:
             print("Wrong media Id.\n")
     
-    def authenticate_admin(self, mainService):
+    def authenticate_admin(self):
         print("Please introduce the token of the administrator: \n")
         admin_token = input()
 
         if admin_token != "":
             try:
-                authenticator = mainService.getAuthenticator()
+                authenticator = self.mainService.getAuthenticator()
                 if authenticator.isAdmin(admin_token) == True:
                     print("Successful administrator authentication")
                 else:
@@ -138,7 +139,7 @@ class Client(Ice.Application):
         else:
             print("You have entered an invalid administrator token.\n") 
 
-    def add_user(self, mainService):
+    def add_user(self):
         print("The choosen option is: add_user. Please introduce the token of the administrator:\n")
         admin_token = input()
         print("Now introduce the user name of the new user: \n")
@@ -147,7 +148,7 @@ class Client(Ice.Application):
         hash_user_psswd = hashlib.sha256(user_psswd.encode()).hexdigest()
         if user_name != "" or user_psswd !="":
             try:
-                authenticator = mainService.getAuthenticator()
+                authenticator = self.mainService.getAuthenticator()
                 authenticator.addUser(user_name, hash_user_psswd, admin_token)
                 print("Succesfull operation. User {user_name} added.\n")
             
@@ -159,7 +160,7 @@ class Client(Ice.Application):
         else:
             print("You have entered an invalid user name or password.\n")
 
-    def remove_user(self, mainService):
+    def remove_user(self):
         print("The choosen option is: remove_user. Please introduce the token of the administrator:\n")
         admin_token = input()
         print("Now introduce the user name of the user you want remove: \n")
@@ -167,7 +168,7 @@ class Client(Ice.Application):
 
         if user_name != "":
             try:
-                authenticator = mainService.getAuthenticator()
+                authenticator = self.mainService.getAuthenticator()
                 authenticator.removeUser(user_name, admin_token)
                 print("Succesfull operation. User {user_name} removed.\n")
             
@@ -179,8 +180,47 @@ class Client(Ice.Application):
         else:
             print("You have entered an invalid user name.\n")
 
+    def rename_media(self):
+        print("The choosen option is: rename_media. Please introduce the token of the administrator:\n \n");
+        admin_token = input()
+        print("Now introduce the id of the media you want to rename: \n")
+        media_id= input()
+        print("Introduce the new name for this media id:")
+        media_newname= input()
+        if media_id != "" and media_newname !="":
+            try:
+                catalog = self.mainService.getCatalog()
+                if self.mainService:
+                    catalog.renameTile(media_id, media_newname, admin_token)
+                    print("Renaming title...\n Title rename successfully\n")
+            except IceFlix.Unauthorized:
+                print("You are not authorized cause the introduced token is not an administrator.\n")
 
+            except IceFlix.WrongMediaId:
+                print("Wrong media Id.\n")
 
+    def upload_file(self):
+        print("The choosen option is: upload_file. \n Please introduce the administrator token:")
+        admin_token = input()
+        print("Introduce the path of the file you want to upload:")
+        file_path= input()
+        try:
+            #file_uploader=
+            file_service= self.mainService.getFileService()
+            #file_service.uploadFile(file_uploader, admin_token)
+        except IceFlix.Unauthorized:
+            print("You are not authorized cause the introduced token is not an administrator.\n")
+    def remove_file(self):
+        print("The choosen option is: remove_file. \n Please introduce the administrator token:")
+        admin_token = input()
+        print("Introduce the media id you want to remove:")
+        media_id= input()
+        try:
+            file_service= self.mainService.getFileService()
+            file_service.removeFile(media_id,admin_token)
+        except IceFlix.Unauthorized:
+            print("You are not authorized cause the introduced token is not an administrator.\n")
+           
 
     
 class AdministratorShell(cmd.Cmd):
