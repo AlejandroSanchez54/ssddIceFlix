ec9e880f16de74ba923e717cd91156b96ea203d2
AlejandroSanchez54 <115221762+AlejandroSanchez54@users.noreply.github.com>
2023-01-20 19:48:17 +0100

    Added final cli

--- iceflix/cli.py
+++ iceflix/cli.py
@@ -3,21 +3,21 @@
 import logging
 import sys
 from iceflix.client import Client
-from iceflix.main import MainApp
 
 
-LOG_FORMAT = '%(asctime)s - %(levelname)-7s - %(module)s:%(funcName)s:%(lineno)d - %(message)s'
 
+LOG_FORMAT = '%(asctime)s' +' ** %(levelname)s **' + '%(message)s'
 
 def setup_logging():
     """Configure the logging."""
     logging.basicConfig(
         level=logging.DEBUG,
         format=LOG_FORMAT,
+        datefmt='%Y-%m-%d %H:%M:%S'
     )
 
 def client():
     """Handles the IceFlix client CLI command."""
     setup_logging()
-    logging.info("Starting IceFlix client...")
+    logging.info(" Starting IceFlix client...")
     sys.exit(Client().main(sys.argv))
